63abaa7d801d84f511377fcae6688077c06f6f9e
Pedro Passos <pedro.passos081@gmail.com>
2025-11-10 10:21:03 -0300

    Adicionando configuração para o strapi cloud

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Ambiente local (Strapi rodando em http://localhost:1337)
       {
-        protocol: "http", // use "https" se for https
+        protocol: "http",
         hostname: "localhost",
         port: "1337",
         pathname: "/uploads/**",
       },
-      // se hospedar o Strapi, adicione outro pattern aqui
+      // Strapi Cloud (seu projeto)
+      {
+        protocol: "https",
+        hostname: "playful-unity-868528a03e.strapiapp.com",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
